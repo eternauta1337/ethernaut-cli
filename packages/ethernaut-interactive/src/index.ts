fba1b886a78ff2683af4468cff417d01e3947c9e
Alejandro <palebluedot@gmail.com>
2024-01-10 07:04:37 -0300

    More tidy ups

--- packages/ethernaut-interactive/src/index.ts
+++ packages/ethernaut-interactive/src/index.ts
@@ -1,16 +1,10 @@
 import { extendEnvironment, task } from "hardhat/config";
-import {
-  HardhatRuntimeEnvironment,
-  ScopeDefinition,
-  ScopesMap,
-  TaskDefinition,
-  TasksMap,
-} from "hardhat/types";
-// TODO: It seems like enquirer is not exporting these types
-// See: https://github.com/enquirer/enquirer/issues/448
-// @ts-ignore
-import { Input } from "enquirer";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { pickTask } from "./internal/task-browser";
+import {
+  makeTasksInteractive,
+  makeScopesInteractive,
+} from "./internal/interactive-task";
 
 extendEnvironment((hre) => {
   hre.previousScope = hre;
@@ -33,92 +27,4 @@ function overrideHelpTask(hre: HardhatRuntimeEnvironment) {
       }
     }
   );
-}
-
-function makeTaskInteractive(
-  taskDefinition: TaskDefinition,
-  hre: HardhatRuntimeEnvironment
-) {
-  if (taskDefinition.isSubtask) {
-    return;
-  }
-
-  // Define the overriding action
-  const action = async (
-    taskArgs: any,
-    hre: HardhatRuntimeEnvironment,
-    runSuper: any
-  ) => {
-    // console.log('Overriding task:', taskDefinition.name);
-    // console.log('Original taskArgs:', taskArgs);
-    // console.log(taskDefinition);
-
-    const newParams = await collectParameters(taskDefinition);
-    // console.log('New params:', newParams);
-
-    Object.entries(newParams).forEach(([name, value]) => {
-      taskArgs[name] = value;
-    });
-    // console.log('New taskArgs:', taskArgs);
-
-    await runSuper(taskArgs, hre, runSuper);
-  };
-
-  // Finalize the override by calling the task in scope
-  if (taskDefinition.scope !== undefined) {
-    const scope = hre.scopes[taskDefinition.scope];
-    scope.task(taskDefinition.name, taskDefinition.description, action);
-  } else {
-    task(taskDefinition.name, taskDefinition.description, action);
-  }
-}
-
-async function collectParameters(taskDefinition: TaskDefinition) {
-  const positionalParamDefinitions = taskDefinition.positionalParamDefinitions;
-  const regularParamDefinitions = Object.values(
-    taskDefinition.paramDefinitions
-  );
-  const paramDefinitions = positionalParamDefinitions.concat(
-    regularParamDefinitions
-  );
-
-  const newParams: { [name: string]: any } = {};
-
-  for (let paramDefinition of paramDefinitions) {
-    // console.log(paramDefinition);
-
-    const prompt = new Input({
-      message: `Enter ${paramDefinition.name}`,
-      initial: paramDefinition.defaultValue,
-    });
-
-    const response = await prompt.run();
-
-    // TODO: Parse and validate using paramDefinition.type
-    // let value = response;
-    // if (paramDefinition.type) {
-    //   value = paramDefinition.type.parse(value);
-    // }
-
-    newParams[paramDefinition.name] = response;
-  }
-
-  return newParams;
-}
-
-function makeTasksInteractive(tasks: TasksMap, hre: HardhatRuntimeEnvironment) {
-  for (let taskName in tasks) {
-    const taskDefinition = tasks[taskName];
-    makeTaskInteractive(taskDefinition, hre);
-  }
-}
-
-function makeScopesInteractive(
-  scopes: ScopesMap,
-  hre: HardhatRuntimeEnvironment
-) {
-  for (let scopeName in scopes) {
-    const scopeDefinition = scopes[scopeName];
-    makeTasksInteractive(scopeDefinition.tasks, hre);
-  }
 }
---
+++ packages/ethernaut-interactive/src/internal/interactive-task.ts
@@ -0,0 +1,97 @@
+import { task } from "hardhat/config";
+import {
+  HardhatRuntimeEnvironment,
+  ScopesMap,
+  TaskDefinition,
+  TasksMap,
+} from "hardhat/types";
+// TODO: It seems like enquirer is not exporting these types
+// See: https://github.com/enquirer/enquirer/issues/448
+// @ts-ignore
+import { Input } from "enquirer";
+
+function makeTaskInteractive(
+  taskDefinition: TaskDefinition,
+  hre: HardhatRuntimeEnvironment
+) {
+  // Subtasks are not meant to be called from the cli
+  if (taskDefinition.isSubtask) {
+    return;
+  }
+
+  // Override the action so that we can
+  // collect parameters from the user
+  const action = async (
+    taskArgs: any,
+    hre: HardhatRuntimeEnvironment,
+    runSuper: any
+  ) => {
+    const newParams = await collectParameters(taskDefinition);
+
+    Object.entries(newParams).forEach(([name, value]) => {
+      taskArgs[name] = value;
+    });
+
+    await runSuper(taskArgs, hre, runSuper);
+  };
+
+  // Complete the override by calling task
+  // at hre or scope level
+  if (taskDefinition.scope !== undefined) {
+    const scope = hre.scopes[taskDefinition.scope];
+    scope.task(taskDefinition.name, taskDefinition.description, action);
+  } else {
+    task(taskDefinition.name, taskDefinition.description, action);
+  }
+}
+
+async function collectParameters(taskDefinition: TaskDefinition) {
+  // Put all params in an array
+  const positionalParamDefinitions = taskDefinition.positionalParamDefinitions;
+  const regularParamDefinitions = Object.values(
+    taskDefinition.paramDefinitions
+  );
+  const paramDefinitions = positionalParamDefinitions.concat(
+    regularParamDefinitions
+  );
+
+  // Prompt the user for each param
+  const newParams: { [name: string]: any } = {};
+  for (let paramDefinition of paramDefinitions) {
+    const prompt = new Input({
+      message: `Enter ${paramDefinition.name}`,
+      initial: paramDefinition.defaultValue,
+    });
+
+    const response = await prompt.run();
+
+    // TODO: Parse and validate using paramDefinition.type
+    // let value = response;
+    // if (paramDefinition.type) {
+    //   value = paramDefinition.type.parse(value);
+    // }
+
+    newParams[paramDefinition.name] = response;
+  }
+
+  return newParams;
+}
+
+function makeTasksInteractive(tasks: TasksMap, hre: HardhatRuntimeEnvironment) {
+  for (let taskName in tasks) {
+    const taskDefinition = tasks[taskName];
+    makeTaskInteractive(taskDefinition, hre);
+  }
+}
+
+function makeScopesInteractive(
+  scopes: ScopesMap,
+  hre: HardhatRuntimeEnvironment
+) {
+  for (let scopeName in scopes) {
+    const scopeDefinition = scopes[scopeName];
+    makeTasksInteractive(scopeDefinition.tasks, hre);
+  }
+}
+
+export { makeTasksInteractive, makeScopesInteractive };
