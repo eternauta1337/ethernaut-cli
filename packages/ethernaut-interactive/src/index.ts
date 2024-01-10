aa2b62e3722078c9b80bc0ab5c2afcef963dfd8e
Alejandro <palebluedot@gmail.com>
2024-01-10 13:22:53 -0300

    Attempt to rescope hh tasks

--- packages/ethernaut-interactive/src/index.ts
+++ packages/ethernaut-interactive/src/index.ts
@@ -1,8 +1,14 @@
 import { extendEnvironment } from "hardhat/config";
 import { makeAllTasksInteractive } from "./internal/interactive-task";
+import { bundleHardhatTasksInScope } from "./internal/default-scope";
 
+// Navigation entry point
 import "./tasks/navigate";
 
 extendEnvironment((hre) => {
+  // An interactive task queries for parameters,
+  // and allows to be ran without required parameters
   makeAllTasksInteractive(hre);
+
+  bundleHardhatTasksInScope(hre);
 });
---
+++ packages/ethernaut-interactive/src/scopes/hh.ts
@@ -0,0 +1,5 @@
+import { scope } from "hardhat/config";
+
+const hh = scope("hh", "Hardhat default tasks");
+
+export { hh };
