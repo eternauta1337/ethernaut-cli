191a9cb2106dc9887f2a6ffd12b698bb52acd06c
Alejandro <palebluedot@gmail.com>
2024-01-11 19:32:50 -0300

    Loading oz ethernaut challenges

--- packages/ethernaut-play/src/tasks/list.ts
+++ packages/ethernaut-play/src/tasks/list.ts
@@ -1,7 +1,32 @@
 import { play } from '../scopes/play';
+// TODO: It seems like enquirer is not exporting these types
+// See: https://github.com/enquirer/enquirer/issues/448
+// @ts-ignore
+import { Select } from 'enquirer';
+import fs from 'fs';
+import path from 'path';
 
 play
   .task('list', 'Lists all Ethernaut levels')
   .setAction(async (taskArgs, hre) => {
-    console.log('Listing levels...');
+    const gamedata = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/gamedata.json'), 'utf8')).levels;
+
+    const choices = gamedata.map((level: any) => {
+      return level.name
+    });
+
+    const prompt = new Select({
+      name: 'level',
+      message: 'Pick a level',
+      limit: 10,
+      choices
+    });
+
+    const response = await prompt.run()
+
+    const level = gamedata.find((level: any) => level.name === response);
+    console.log(level);
+
+    const info = fs.readFileSync(path.join(__dirname, '../data/levels', `${level.description}`), 'utf8');
+    console.log(info)
   });
