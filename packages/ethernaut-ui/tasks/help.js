fadfa5239020d4624c453472dbc941e60090f125
Alejandro <palebluedot@gmail.com>
2024-02-06 11:00:15 -0300

    Jump straight into a scope

--- packages/ethernaut-ui/tasks/help.js
+++ packages/ethernaut-ui/tasks/help.js
@@ -2,5 +2,15 @@ const { task } = require('hardhat/config');
 const navigateFrom = require('../internal/navigate-from');
 
 task('help', 'Jumps into the help navigator').setAction(async ({}, hre) => {
-  await navigateFrom(hre);
+  let node = hre;
+
+  if (process.argv.length >= 3) {
+    const scope = process.argv[2];
+    node = hre.scopes[scope];
+    if (node === undefined) {
+      throw new Error(`Unknown scope: ${scope}`);
+    }
+  }
+
+  await navigateFrom(node);
 });
