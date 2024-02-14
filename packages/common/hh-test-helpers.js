5adbce89d73226fb3b4c2a5bd3a5099205bc4e90
Alejandro <palebluedot@gmail.com>
2024-02-14 18:03:47 -0300

    Preparing for tests in toolbox

---
+++ packages/common/hh-test-helpers.js
@@ -0,0 +1,10 @@
+function useFixtureProject(projectName) {
+  console.log('>', projectName);
+  // before(function () {
+  //   this.fixture = fixtureName;
+  // });
+}
+
+module.exports = {
+  useFixtureProject,
+};
---
+++ packages/ethernaut-toolbox/test/tasks/to-bytes.test.js
@@ -0,0 +1,6 @@
+const { useFixtureProject } = require('common/hh-test-helpers');
+
+describe('to-bytes', function () {
+  useFixtureProject('project-with-scripts');
+  // useEnvironment();
+});
