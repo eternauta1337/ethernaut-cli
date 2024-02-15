84c47ec8bf75bff6b03f71c325196b067ded71f0
Alejandro <palebluedot@gmail.com>
2024-02-14 22:47:01 -0300

    Tests for unit task

---
+++ packages/ethernaut-toolbox/test/tasks/unit.test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const { useEnvironment, collectOutput } = require('common/test-helpers');
+
+describe('unit', function () {
+  useEnvironment('basic-project');
+  const output = collectOutput();
+
+  it('converts 1 ether to wei', async function () {
+    await this.hre.run(
+      { scope: 'util', task: 'unit' },
+      {
+        value: '1',
+        from: 'ether',
+        to: 'wei',
+      }
+    );
+    assert.equal(output(), '1000000000000000000');
+  });
+
+  it('converts 1 wei to ether', async function () {
+    await this.hre.run(
+      { scope: 'util', task: 'unit' },
+      {
+        value: '1',
+        from: 'wei',
+        to: 'ether',
+      }
+    );
+    assert.equal(output(), '0.000000000000000001');
+  });
+
+  it('converts 12 szabo to mwei', async function () {
+    await this.hre.run(
+      { scope: 'util', task: 'unit' },
+      {
+        value: '12',
+        from: 'szabo',
+        to: 'mwei',
+      }
+    );
+    assert.equal(output(), '12000000');
+  });
+});
