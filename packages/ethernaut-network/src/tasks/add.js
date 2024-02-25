dfcffe1c652d1550bfca2dc0b10a73a03ec1b264
Alejandro <palebluedot@gmail.com>
2024-02-24 23:33:40 -0300

    Introduce network add task

--- packages/ethernaut-network/src/tasks/add.js
+++ packages/ethernaut-network/src/tasks/add.js
@@ -1,18 +1,65 @@
 const { types } = require('hardhat/config')
 const output = require('common/src/output')
+const fs = require('fs')
+const stringify = require('javascript-stringify').stringify
 
 require('../scopes/net')
   .task('add', 'Adds a network to the cli')
   .addOptionalParam(
     'alias',
-    'The name of the new network',
+    'How the network will be referenced from the cli',
     undefined,
     types.string,
   )
-  .setAction(async ({ alias }) => {
+  .addOptionalParam(
+    'name',
+    'The actual name of the underlying network',
+    undefined,
+    types.string,
+  )
+  .addOptionalParam(
+    'provider',
+    'The url of the network provider, e.g. https://ethereum-rpc.publicnode.com. Note: Environment variables may be included, e.g. https://eth-mainnet.alchemyapi.io/v2/process.env.ALCHEMY_API_KEY. Make sure to specify these in your .env file.',
+    undefined,
+    types.string,
+  )
+  .setAction(async ({ alias, name, provider }, hre) => {
     try {
+      if (alias in hre.userConfig.networks) {
+        throw new Error(`Network ${alias} already exists`)
+      }
+
+      const newConfig = JSON.parse(JSON.stringify(hre.userConfig))
+      if (!newConfig.networks) {
+        newConfig.networks = {}
+      }
+
+      newConfig.networks[alias] = {
+        name,
+        url: provider,
+      }
+
+      saveConfig(newConfig, hre)
+
       output.resultBox(`Added network ${alias}`)
     } catch (err) {
       return output.errorBox(err)
     }
   })
+
+function saveConfig(newConfig, hre) {
+  const filePath = hre.config.paths.configFile
+  let fileContent = fs.readFileSync(filePath, 'utf-8')
+
+  // Find the module.exports part of the file
+  let regex = /(module\.exports = [\s\S]*)/
+  if (!regex.test(fileContent)) {
+    throw new Error('Could not find the module.exports object in the file.')
+  }
+
+  // Rebuild the file with the new exports object
+  const newModuleExports = `module.exports = ${stringify(newConfig, null, 2)}`
+  fileContent = fileContent.replace(regex, newModuleExports)
+
+  fs.writeFileSync(filePath, fileContent)
+}
