c37e66fd96dcde1a9bb47ac3766d46e166c774a3
Alejandro <palebluedot@gmail.com>
2024-02-26 10:40:10 -0300

    Add a task for listing networks

---
+++ packages/ethernaut-network/src/tasks/list.js
@@ -0,0 +1,18 @@
+const output = require('common/src/output')
+const storage = require('../internal/storage')
+
+require('../scopes/net')
+  .task('list', 'Prints all networks')
+  .setAction(async () => {
+    try {
+      const networks = storage.readNetworks()
+      const strs = []
+      Object.entries(networks).forEach(([name, network]) => {
+        if (name === 'activeNetwork') return
+        strs.push(`- ${name} (${network.url})`)
+      })
+      output.resultBox(strs.join('\n'))
+    } catch (err) {
+      return output.errorBox(err)
+    }
+  })
---
+++ packages/ethernaut-network/test/tasks/list.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const { Terminal } = require('common/src/terminal')
+const storage = require('../../src/internal/storage')
+
+describe('list', function () {
+  const terminal = new Terminal()
+
+  before('add some networks', async function () {
+    const networks = storage.readNetworks()
+    if (!('test__3' in networks)) networks.test__3 = { url: 'poop' }
+    if (!('test__4' in networks)) networks.test__4 = { url: 'poop' }
+    storage.storeNetworks(networks)
+  })
+
+  after('remove networks', async function () {
+    const networks = storage.readNetworks()
+    if ('test__3' in networks) delete networks.test__3
+    if ('test__4' in networks) delete networks.test__4
+    storage.storeNetworks(networks)
+  })
+
+  describe('when calling list', function () {
+    before('call', async function () {
+      await terminal.run('npx hardhat net list')
+    })
+
+    it('prints the networks', async function () {
+      assert.ok(terminal.output.includes('- test__3 (poop)'))
+      assert.ok(terminal.output.includes('- test__4 (poop)'))
+    })
+  })
+})
