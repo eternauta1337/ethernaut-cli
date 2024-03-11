201536acc95dc0ec3b3a66fab7d38feb0147d2b2
Alejandro <palebluedot@gmail.com>
2024-03-11 06:31:31 -0300

    Improve ai instructions

--- packages/ethernaut-challenges/src/index.js
+++ packages/ethernaut-challenges/src/index.js
@@ -17,9 +17,11 @@ extendEnvironment((hre) => {
 })
 
 extendConfig((config) => {
+  const challengesConfig = require('./internal/assistants/configs/challenges.json')
+
   if (config.ethernaut?.ai) {
     config.ethernaut.ai.interpreter.additionalInstructions.push(
-      'If asked to complete ethernaut challenges use the info command to get information about the level, including how to play it, where to find the abi, the instances Solidity source code, etc. Make sure to look at the source carefully. Use the instance command to produce an instance of the level. Do not recommend using the web dapp of the game. Always play it using the interact package of this cli application. Try to solve the level yourself using the cli tools. Do not attempt to run a script with the run command, or compile any contracts. You may provide code for a javascript script that the user may run manually. If so, use the ethers library instead of web3. You will be able to sign transactions with the call command and always have ether to play. Once a level instance has been manipulated to the satisfaction of the level instructions, make sure to submit the instance with the instance command',
+      challengesConfig.additionalInstructions,
     )
   }
 })
