772c362632185df54a56be6e495558e0d4af50a0
Alejandro <palebluedot@gmail.com>
2024-03-13 08:04:38 -0300

    Dedup interact ui extensions

--- packages/ethernaut-interact-ui/src/prompts/paramsERC20.js
+++ packages/ethernaut-interact-ui/src/prompts/paramsERC20.js
@@ -1,27 +1,8 @@
-const loadAbi = require('ethernaut-interact/src/tasks/contract/load-abi')
-const debug = require('ethernaut-common/src/debug')
-const prompt = require('ethernaut-common/src/prompt')
 const storage = require('ethernaut-interact/src/internal/storage')
+const paramsPrompt = require('./params')
 
 module.exports = async function promptParamsERC20({ fn }) {
-  try {
-    const _abi = loadAbi(storage.findAbi('ERC20'))
+  const abi = storage.findAbi('ERC20')
 
-    const fnName = fn.split('(')[0]
-    const abiFn = _abi.find((abiFn) => abiFn.name === fnName)
-
-    let params = []
-    for (const input of abiFn.inputs) {
-      const response = await prompt({
-        type: 'input',
-        message: `Enter ${input.name} (${input.type})`,
-      })
-
-      params.push(response)
-    }
-
-    return params.join(',')
-  } catch (err) {
-    debug.log(err, 'ui')
-  }
+  return await paramsPrompt({ abi, fn })
 }
