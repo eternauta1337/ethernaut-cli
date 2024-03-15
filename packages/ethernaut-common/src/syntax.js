7d58c8351ae54f9bc162191eb2f74e050d93e1ec
Alejandro <palebluedot@gmail.com>
2024-03-14 23:32:56 -0300

    Trying to catch a slippery one

--- packages/ethernaut-common/src/syntax.js
+++ packages/ethernaut-common/src/syntax.js
@@ -1,6 +1,12 @@
 const camelToKebabCase = require('ethernaut-common/src/kebab')
+const debug = require('ethernaut-common/src/debug')
 
 module.exports = function toCliSyntax(task, args) {
+  debug.log(
+    `Converting to CLI syntax: ${task.name}, args: ${JSON.stringify(args)}`,
+    'common',
+  )
+
   const tokens = ['ethernaut']
   if (task.scope) tokens.push(task.scope)
   tokens.push(task.name)
@@ -13,7 +19,9 @@ module.exports = function toCliSyntax(task, args) {
     if (paramDef) isOption = true
     else paramDef = task.positionalParamDefinitions.find((p) => p.name === name)
     if (!paramDef) {
-      throw new Error(`No definition found for parameter ${name}`)
+      throw new Error(
+        `No definition found for parameter ${name}, and task ${task.name}, with args ${JSON.stringify(args, null, 2)}`,
+      )
     }
 
     const isFlag = paramDef.isFlag
