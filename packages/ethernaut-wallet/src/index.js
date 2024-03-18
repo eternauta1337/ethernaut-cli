e6c75c45ce869d1227b4569d0b13b1dd36129a2e
Alejandro <palebluedot@gmail.com>
2024-03-18 05:05:09 -0500

    Add wallet ai instructions

--- packages/ethernaut-wallet/src/index.js
+++ packages/ethernaut-wallet/src/index.js
@@ -1,4 +1,4 @@
-const { extendEnvironment } = require('hardhat/config')
+const { extendEnvironment, extendConfig } = require('hardhat/config')
 const requireAll = require('ethernaut-common/src/io/require-all')
 const spinner = require('ethernaut-common/src/ui/spinner')
 const storage = require('./internal/storage')
@@ -25,4 +25,14 @@ extendEnvironment((hre) => {
   })
 
   ensureActiveSigner()
+})
+
+extendConfig((config) => {
+  const walletConfig = require('./internal/assistants/configs/wallet.json')
+
+  if (config.ethernaut?.ai) {
+    config.ethernaut.ai.interpreter.additionalInstructions.push(
+      walletConfig.additionalInstructions,
+    )
+  }
 })
