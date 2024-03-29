02925b1172e0bb90b8eb7c64c74558924369b162
Alejandro <palebluedot@gmail.com>
2024-03-29 08:46:32 -0300

    More advanced abi prompt tests

--- packages/ethernaut-interact-ui/test/prompt/abi.test.js
+++ packages/ethernaut-interact-ui/test/prompt/abi.test.js
@@ -1,4 +1,4 @@
-const { Terminal } = require('ethernaut-common/src/test/terminal')
+const { Terminal, keys } = require('ethernaut-common/src/test/terminal')
 
 describe('abi prompt', function () {
   const terminal = new Terminal()
@@ -29,14 +29,68 @@ describe('abi prompt', function () {
     describe('when an address is provided', function () {
       before('call', async function () {
         await terminal.run(
-          'npx hardhat interact contract --address 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720 --network mainnet',
-          2000,
+          'npx hardhat interact contract --address 0xdAC17F958D2ee523a2206206994597C13D831ec7 --network mainnet',
+          4000,
         )
       })
 
       it('provides the option to fetch abi from etherscan', async function () {
         terminal.has('Fetch from Etherscan')
       })
+
+      describe('when etherscan is selected', function () {
+        before('nav', async function () {
+          await terminal.input(keys.DOWN)
+          await terminal.input(keys.DOWN)
+          await terminal.input(keys.DOWN)
+          await terminal.input(keys.ENTER, 2000)
+        })
+
+        it('finds the TetherToken ABI', async function () {
+          terminal.has('Abi fetched from Etherscan TetherToken')
+        })
+
+        it('displays the fns', async function () {
+          terminal.has('name()')
+          terminal.has('decimals()')
+        })
+      })
+    })
+  })
+
+  describe('when browsing known ABIs', function () {
+    before('call', async function () {
+      await terminal.run('npx hardhat interact contract', 2000)
+    })
+
+    before('nav', async function () {
+      await terminal.input(keys.DOWN)
+      await terminal.input(keys.DOWN)
+      await terminal.input(keys.ENTER)
+    })
+
+    it('shows known ABIs', async function () {
+      terminal.has('CoinFlip')
+      terminal.has('DelegateERC20')
+    })
+  })
+
+  describe('when browsing the fs', function () {
+    before('call', async function () {
+      await terminal.run('npx hardhat interact contract', 2000)
+    })
+
+    before('nav', async function () {
+      await terminal.input(keys.DOWN)
+      await terminal.input(keys.ENTER)
+    })
+
+    it('shows back nav', async function () {
+      terminal.has('..')
+    })
+
+    it('shows home dir abbreviation', async function () {
+      terminal.has('~')
     })
   })
 })
