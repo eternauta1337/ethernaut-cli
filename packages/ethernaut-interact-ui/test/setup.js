45bd3cfa1b5c9c4d5d0bf0a3e7f605a0de04533c
Alejandro <palebluedot@gmail.com>
2024-04-01 09:05:59 -0300

    ci

--- packages/ethernaut-interact-ui/test/setup.js
+++ packages/ethernaut-interact-ui/test/setup.js
@@ -2,6 +2,3 @@ const { loadProject, muteOutput } = require('ethernaut-common/src/test/setup')
 
 muteOutput()
 loadProject('test/fixture-projects/basic-project')
-
-// Fixes nyc not collecting coverage from subprocesses
-require('../../ethernaut-interact/src/index')
