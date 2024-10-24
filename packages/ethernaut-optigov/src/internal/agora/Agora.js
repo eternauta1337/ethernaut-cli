591c7691e3cc83f5cedc0254aa1e5c9ae01c24dc
Luis Videla <luisvid@gmail.com>
2024-10-24 07:55:54 -0300

    refactor: Update Agora class and related modules Auth & Projects

--- packages/ethernaut-optigov/src/internal/agora/Agora.js
+++ packages/ethernaut-optigov/src/internal/agora/Agora.js
@@ -1,34 +1,61 @@
 const axios = require('axios')
-const debug = require('ethernaut-common/src/ui/debug')
 const EthernautCliError = require('ethernaut-common/src/error/error')
-const Auth = require('./Auth')
-const Projects = require('./Projects')
+const debug = require('ethernaut-common/src/ui/debug')
 
 const API_BASE_URL = 'https://vote.optimism.io/api/v1'
 const AGORA_API_KEY = process.env.AGORA_API_KEY
 
 class Agora {
   constructor() {
-    this.auth = new Auth(this)
-    this.projects = new Projects(this)
+    this.apiKey = AGORA_API_KEY
+    this.apiBaseUrl = API_BASE_URL
+    this.bearerToken = null
   }
 
-  async getSpec() {
-    try {
-      const response = await axios.get(`${API_BASE_URL}/spec`, {
-        headers: {
-          Authorization: `Bearer ${AGORA_API_KEY}`,
-        },
-      })
+  // Axios instance setup
+  createAxiosInstance() {
+    const headers = {
+      Authorization: this.bearerToken
+        ? `Bearer ${this.bearerToken}`
+        : `Bearer ${this.apiKey}`,
+    }
 
-      debug.log(`Spec: ${response.data}`, 'ethernaut-optigov')
-      return response.data
-    } catch (error) {
+    return axios.create({
+      baseURL: this.apiBaseUrl,
+      headers,
+    })
+  }
+
+  // Handle common API errors
+  handleError(error) {
+    if (error.response) {
+      throw new EthernautCliError(
+        'ethernaut-optigov',
+        `Http status error: ${error.response.data}`,
+      )
+    } else {
       throw new EthernautCliError(
         'ethernaut-optigov',
         `Http status error: ${error.message}`,
       )
     }
+  }
+
+  // common method for getting API spec
+  async getSpec() {
+    try {
+      const axiosInstance = this.createAxiosInstance()
+      const response = await axiosInstance.get('/spec')
+      debug.log(`Spec: ${response.data}`, 'ethernaut-optigov')
+      return response.data
+    } catch (error) {
+      this.handleError(error)
+    }
+  }
+
+  // Set the bearer token after authentication
+  setBearerToken(token) {
+    this.bearerToken = token
   }
 }
 
